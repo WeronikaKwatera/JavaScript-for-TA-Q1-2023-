262abfc9e8a3c68c1fcd88cbbad9791cfe7a0fed
Weronika <wkwat@softserveinc.com>
2023-03-13 21:32:33 +0100

    homework1

---
+++ homework1/emailAndPassword.js
@@ -0,0 +1,7 @@
+let email = prompt("What is your email?", "example@mail.com");
+let password = prompt("What is your password?", "Qwerty098*");
+alert(email);
+alert(password);
+document.write(
+  "dear user you email is " + email + "and your password is " + password
+);
